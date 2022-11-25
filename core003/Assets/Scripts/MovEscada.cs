8875ad94163e46bc1e84f621d4e35110febec829
Rafael Henrique Melo Batista <batista.h@academico.ifrn.edu.br>
2022-11-25 19:46:01 -0300

    25/11

---
+++ core003/Assets/Scripts/MovEscada.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovEscada : MonoBehaviour
+{
+    private float vertical; //Sera o input
+    private float speed = 8f; //A velocidade de subir a escada;
+    private bool escada; 
+    private bool escalando;
+
+    public Rigidbody2D playerRb;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        vertical = Input.GetAxis("Vertical");// Input para subir a escada
+
+        if (escada && Mathf.Abs(vertical) > 0f)
+        {
+            escalando = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //Desativando a gravidade para o player subir
+        if (escalando == true)
+        {
+            playerRb.gravityScale = 0f;
+            playerRb.velocity = new Vector2(playerRb.velocity.x, vertical * speed);
+        }
+        //Ativando a gravidade novamente
+        else
+        {
+            playerRb.gravityScale = 2f;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("escada"))//ESSA ESCADA É A DA TEG
+        {
+            escada = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.CompareTag("escada"))
+        {
+            escada = false;
+            escalando = false;
+        }
+    }
+}
--- core003/Assets/Scripts/PlayerController.cs
+++ core003/Assets/Scripts/PlayerController.cs
@@ -1,76 +1,86 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody2D rb;
-    public float speedMax;
-    public float speed;
-    public float forca = 5;
-    public bool isGround;
-    public bool isDash = false;
-    public float tempoDash = 10;
-    public float tempoAtual;
-    public int mult = 2;
+    public float speed; // Responsavel pela velocidade do player
+    public Rigidbody2D playerRb;
+    public float jumpForce; //Sera responsavel pelo força do pulo
+    public bool pulo;
+    public bool isgrounded;
     
-    public string ultima = "direita";
-
-    bool flipX;
-
-
-    // Start is called before the first frame update
+    
+    
+    
+    private float movePlayer;// Sera responsavel pelo INPUT do player
+    private bool porta;
+    private GameObject novaPorta;
+    
+    
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        
-
+        novaPorta = GameObject.Find("novaPorta");
     }
 
-    
     void Update()
     {
-        //rb.velocity = transform.right * Time.deltaTime * speed;
-        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * Time.deltaTime * speed, rb.velocity.y);
+        novaPosicao();
+        movePlayer = Input.GetAxis("Horizontal"); //Usando o input para atribuir o teclado com os comandos da horizontal.
+        playerRb.velocity = new Vector2(movePlayer * speed, playerRb.velocity.y); //Movimentando o player, para um lado e para o outro... tanto para a esquerda, quanto para a direita.
+        
+        pulo = Input.GetButtonDown("Jump");//Quando os botoes de Jump.
+
+        //Condiçao do pulo
+        if (pulo == true && isgrounded == true)
+        {
+            playerRb.AddForce(new Vector2(0, jumpForce));
+            isgrounded = false;
+        }
+
+        //Se o movimento do player estiver andando para direita, ele ira continuar andando para a direita
+        if (movePlayer > 0)
+        {
+            transform.eulerAngles = new Vector2(0, 0);
+        }
+        
+        //Se o movimento do player estiver andando para esquerda, fara 180 graus para a esquerda
+        if (movePlayer < 0)
+        {
+            transform.eulerAngles = new Vector2(0, 180);
+        }
+        
+        
         
 
-        if (Input.GetKey(KeyCode.Space) && isGround)
-        {
-            rb.AddForce(transform.up * forca);
-            isGround = false;
-        }
-
-        if (Input.GetKeyDown(KeyCode.F) && isDash == false)
-        {
-            isDash = true;
-            tempoAtual = tempoDash;
-        }
-
-        if (isDash)
-        {
-            //tudo que acontece no dash é aqui
-            
-            //o tenmpo atual vai se torna o tempo determinado
-            speed = speedMax * mult;
-            
-            tempoAtual -= Time.deltaTime;
-            if (tempoAtual <= 0)
-            {
-                isDash = false;
-                speed = speedMax;
-            }
-
-        }
-
-        
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    //Detectando se o player está na TAG "chao"
+    private void OnCollisionEnter2D(Collision2D col)
     {
-        if (collision.gameObject.layer == 6)
+        if (col.gameObject.CompareTag("chao"))
         {
-            isGround = true;
+            isgrounded = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        //Quando o player colidir com a tag next, ira executar a função novaPosicao.
+        if (col.gameObject.CompareTag("next"))
+        {
+            porta = true;
+        }
+    }
+
+    private void novaPosicao()
+    {
+        if (porta == true)
+        {
+            playerRb.transform.position = new Vector2(novaPorta.transform.position.x, novaPorta.transform.position.y);
+            porta = false;
         }
     }
     
