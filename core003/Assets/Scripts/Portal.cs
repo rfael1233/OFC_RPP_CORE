2d9da5b71d50f6824b252a1524533efb68696339
Rafael Henrique Melo Batista <batista.h@academico.ifrn.edu.br>
2022-12-12 20:47:45 -0300

    12.1/12
    parte da noite

---
+++ core003/Assets/Scripts/Portal.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    public GameObject portalSaida;
+    private GameObject playerGO;
+    
+    
+    void Start()
+    {
+        playerGO = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        playerGO.transform.position = portalSaida.transform.position - new Vector3(0, 0.7f, 0);
+    }
+}
